d8a7416829a44ec2c8a3777ebfb11361b8e71c46
Danish-Baloch <thenish09@gmail.com>
2025-10-25 23:40:13 +0400

    minor changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
+import { Router } from 'express'
 import React from 'react'
+import { Home } from './pages/Home'
+
 
 export const App = () => {
   return (
-    <div classname="text-green-500">  
-      Testing
+    <div classname="mx-4 sm:mx-[10%]">  
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+      </Routes>
+
     </div>
     
   )
